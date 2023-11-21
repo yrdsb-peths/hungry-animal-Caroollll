8e1a42cd5e4f0623e1070fa0ca7333a551e07eac
Caroollll <349351197@gapps.yrdsb.ca>
2023-11-21 08:43:02 -0500

    created seal and made it move

---
+++ Seal.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+public class Seal extends Actor
+{
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("a"))
+        {
+            move(-2);
+        }
+        if(Greenfoot.isKeyDown("d"))
+        {
+            move(2);
+        }
+    }
+}
